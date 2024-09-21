6b330c8257fe502ba6c8a0142702dc5e7232a88a
Rehab <rehabsobhy184@gmail.com>
2024-09-21 18:44:31 +0300

    create splash screen

---
+++ lib/splash.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:therapy_splasher/constants/clors.dart';
+import 'package:therapy_splasher/pageView/presntation/views/on_boarding_page_view.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context) => OnBoardingPageView()));
+    });
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primary,
+      body: Center(
+        child: Container(
+            height: 150,
+            child: Image.asset("assets/images/Therapy Splasher (4).png",
+                fit: BoxFit.cover)),
+      ),
+    );
+  }
+}
